ea83581a6dfaa36d05e2d9b2b9ef5fb743bdaa1d
Mustafa Marzouk <40539669+TopVirusMinus@users.noreply.github.com>
2021-03-02 12:36:04 +0200

    First-Commit 👌

---
+++ app.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
+    audio.loop = true;
+  });
+
+  document.querySelector("audio").loop = true;
